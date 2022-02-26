da62d6b8857f20b14b862a8f74cb9c64e12f6327
jg925 <jg925@cornell.edu>
2022-02-26 18:35:06 -0500

    dynamic site creation

--- app.js
+++ app.js
@@ -4,15 +4,21 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
+const sessionsRouter = require('./src/routers/sessionsRouter');
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('Hello from my app');
+  res.render('index', { title: 'Globomantics', data: ['a', 'b', 'c'] });
 });
+
+app.use('/sessions', sessionsRouter);
 
 app.listen(port, () => {
   debug(`listening on port ${chalk.green(port)}`);
---
+++ src/routers/sessionsRouter.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const sessionsRouter = express.Router();
+const sessions = require('../data/sessions.json');
+
+sessionsRouter.route('/').get((req, res) => {
+  res.render('sessions', {
+    sessions,
+  });
+});
+
+sessionsRouter.route('/:id').get((req, res) => {
+  const { id } = req.params;
+  res.render('session', {
+    session: sessions[id],
+  });
+});
+
+module.exports = sessionsRouter;
